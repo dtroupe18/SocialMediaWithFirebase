e04b39aa5ec45813174329a6bd93e4b7d655b47f
Dave <Dave.Troupe@gmail.com>
2017-05-25 10:41:37 -0400

    Create Post Added

--- FakeInstagram/AppDelegate.swift
+++ FakeInstagram/AppDelegate.swift
@@ -13,6 +13,36 @@ import Firebase
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
+    var activityIndicator = UIActivityIndicatorView(activityIndicatorStyle: .whiteLarge)
+    var container: UIView!
+    
+    
+    class func instance() -> AppDelegate {
+        return UIApplication.shared.delegate as! AppDelegate
+    }
+    
+    func showActivityIndicator() {
+        if let window = window {
+            container = UIView()
+            container.frame = window.frame
+            container.center = window.center
+            container.backgroundColor = UIColor(white: 0, alpha: 0.8)
+            
+            activityIndicator.frame = CGRect(x: 0, y: 0, width: 40, height: 40)
+            activityIndicator.hidesWhenStopped = true
+            activityIndicator.center = CGPoint(x: container.frame.size.width / 2, y: container.frame.size.height / 2)
+            
+            container.addSubview(activityIndicator)
+            window.addSubview(container)
+            activityIndicator.startAnimating()
+        }
+    }
+    
+    func dismissActivityIndicator() {
+        if let _ = window {
+            container.removeFromSuperview()
+        }
+    }
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
---
+++ FakeInstagram/UploadViewController.swift
@@ -0,0 +1,93 @@
+//
+//  UploadViewController.swift
+//  FakeInstagram
+//
+//  Created by Dave on 5/25/17.
+//  Copyright © 2017 Dave. All rights reserved.
+//
+
+import UIKit
+import Firebase
+
+class UploadViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+
+    
+    @IBOutlet weak var previewImage: UIImageView!
+    @IBOutlet weak var postButton: UIButton!
+    @IBOutlet weak var selectImage: UIButton!
+    
+    var picker = UIImagePickerController()
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        picker.delegate = self
+    }
+
+    @IBAction func selectImagePressed(_ sender: Any) {
+        picker.allowsEditing = true
+        picker.sourceType = .photoLibrary
+        self.present(picker, animated: true, completion: nil)
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
+        // has to be edited image because we allowed editing
+        if let image = info[UIImagePickerControllerEditedImage] as? UIImage {
+            self.previewImage.image = image
+            selectImage.isHidden = true
+            postButton.isHidden = false
+        }
+        
+        self.dismiss(animated: true, completion: nil)
+    }
+    
+    
+    @IBAction func postPressed(_ sender: Any) {
+        AppDelegate.instance().showActivityIndicator()
+        
+        let uid = Auth.auth().currentUser!.uid
+        let ref = Database.database().reference()
+        let storage = Storage.storage().reference(forURL: "gs://fakeinstagram-83e97.appspot.com")
+        
+        let key = ref.child("posts").childByAutoId().key
+        let imageRef = storage.child("posts").child(uid).child("\(key).jpg")
+        
+        let data = UIImageJPEGRepresentation(self.previewImage.image!, 0.6)
+        
+        let uploadTask = imageRef.putData(data!, metadata: nil) { (metadata, error) in
+            if error != nil {
+                AppDelegate.instance().dismissActivityIndicator()
+                self.displayAlert(title: "Upload Error", message: (error?.localizedDescription)!)
+                return
+            }
+            
+            imageRef.downloadURL(completion: {(url, error) in
+                if let url = url {
+                    let feed = ["userID": uid,
+                                "pathToImage": url.absoluteString,
+                                "likes": 0,
+                                "author": Auth.auth().currentUser!.displayName!,
+                                "postID": key] as [String: Any]
+                    
+                    let postFeed = ["\(key)" : feed]
+                    
+                    ref.child("posts").updateChildValues(postFeed)
+                    AppDelegate.instance().dismissActivityIndicator()
+                    self.dismiss(animated: true, completion: nil)
+                }
+            })
+        }
+        uploadTask.resume()
+    }
+    
+    func displayAlert(title: String, message: String) {
+        let alertController = UIAlertController(title: title, message: message, preferredStyle: .alert)
+        
+        let defaultAction = UIAlertAction(title: "OK", style: .cancel, handler: nil)
+        alertController.addAction(defaultAction)
+        
+        self.present(alertController, animated: true, completion: nil)
+    }
+
+}
